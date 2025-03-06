94181bfaed7e35721141ff9b7b48c8e9a8ad379e
Ana María <moraviedaanamaria@gmail.com>
2025-03-06 16:34:05 -0500

    Agregar botón en Javascript

--- Ejercicio1/ejercicio1.js
+++ Ejercicio1/ejercicio1.js
@@ -1,6 +1,10 @@
 // Se definen los elementos a traer//
 const boton = document.getElementById('boton');
 const contenido = document.getElementById('Contenido');
+const boton2 = document.getElementById('boton2');
+
+//Guardando el contenido del texto//
+const contenidoOriginal = contenido.textContent;
 
 // Se agrega un evento de click al botón y a la vez su funcion //
 boton.addEventListener('click', function() {
@@ -8,5 +12,9 @@ boton.addEventListener('click', function() {
             "Los primeros instrumentos musicales conocidos datan de hace más de 40,000 años. " +
             "Inicialmente, la música estaba ligada a rituales religiosos, ceremonias de caza y comunicación tribal. " +
             "A lo largo de los siglos, la música ha evolucionado, reflejando los cambios culturales, sociales y tecnológicos de cada época."
+});
+
+boton2.addEventListener('click', function(){
+    contenido.textContent = contenidoOriginal;
 });
 
