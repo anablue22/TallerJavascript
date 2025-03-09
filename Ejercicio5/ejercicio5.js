4cfa4f2f256cf098cf2a946d449aa130eb92377c
Ana María <moraviedaanamaria@gmail.com>
2025-03-09 10:25:35 -0500

    Funcionalidad dark mood

---
+++ Ejercicio5/ejercicio5.js
@@ -0,0 +1,4 @@
+//botón con clic//
+document.getElementById("toggle-dark-mode").addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");//selecciona el botón y con la clase lo cambia al modo oscuro//
+}); 
