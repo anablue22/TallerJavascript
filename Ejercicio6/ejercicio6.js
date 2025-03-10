eb6205a3efb1df2b94ffd88d01cc53ff229abf6f
Ana María <moraviedaanamaria@gmail.com>
2025-03-09 21:05:02 -0500

    Corrección de formulario en HTML

---
+++ Ejercicio6/ejercicio6.js
@@ -0,0 +1,8 @@
+let nombre = document.getElementById("nombre");
+let correo = docuemnt.getElementById("correo"); 
+let contraseña = document.getElementById("password"); 
+
+if (correo === "") {
+    alert("Por favor, escribe una correo válido.");
+    return;
+}
